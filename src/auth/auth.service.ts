f59d25da7c6887449a871b6478eec91a1e9308e2
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2023-11-17 12:37:15 +0100

    Inital commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  // login() {}
+  signup() {
+    return 'test';
+  }
+  signin() {
+    return 'test';
+  }
+}
